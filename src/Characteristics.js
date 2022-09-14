556c86b2e9afb2f3867b6ce79746c7ec381f5d92
HouliHeeler <luke_houlihan@hotmail.com>
2022-09-13 17:09:04 -0700

    update statsArray based on input selection

--- src/Characteristics.js
+++ src/Characteristics.js
@@ -1,18 +1,23 @@
 import React, {useState} from 'react'
 
 export default function Characteristics() {
-    let statsArray = [40, 50, 50, 50, 60, 60, 70, 80]
+    let statsArray = ["40", "50", "50", "50", "60", "60", "70", "80"]
 
     let [stat, setStat] = useState("Choose Wisely")
 
     let handleStatChange = (e) => {
+        let valueIndex = statsArray.indexOf(e.target.value)
         setStat(e.target.value)
+        if(e.target.value !== "Choose Wisely") {
+            statsArray.splice(valueIndex, 1)
+            alert(statsArray)
+        }
         }
 
-    const statsInput = <select onChange={handleStatChange}>
-    <option value="Choose Wisely">Choose Wisely</option>
-    {statsArray.map((stats, index) => <option key={index} value={stats}>{stats}</option>)}
-</select>
+    const statsInput =  <select onChange={handleStatChange}>
+                            <option value="Choose Wisely">Choose Wisely</option>
+                            {statsArray.map((stats, index) => <option key={index} value={stats}>{stats}</option>)}
+                        </select>
 
     return (
         <div className='Investigator'>
