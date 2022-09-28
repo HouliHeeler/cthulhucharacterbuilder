e7bdda1448e21b9d41c6d60c5e2fd5ab0cbc39b3
HouliHeeler <luke_houlihan@hotmail.com>
2022-09-27 22:08:19 -0700

    unsuccessfully try to pull id from selected dropdown menu

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -6,6 +6,7 @@ export default function Category(props) {
         <div className='Info--Span'>
             <span>{props.name}</span>
             <Select 
+                    inputId={props.name}
                     defaultValue={props.defaultValue}
                     onChange={props.handleSelect}
                     options={props.options}
--- src/Components/Characteristics.js
+++ src/Components/Characteristics.js
@@ -5,17 +5,22 @@ import {options, statNames} from '../Constants'
 export default function Characteristics() {
     const [selectedOption, setSelectedOption] = useState(options[0]);
     const [statsArray, setStatsArray] = useState([...options])
+    const [strength, setStrength] = useState(null)
+    const [chosenArray, setChosenArray] = useState([])
     function handleChange(e) {
+        setStrength(e.label)
+        setChosenArray(strength)
         let temp = [...statsArray].indexOf(e)
         let newArray = [...statsArray].slice(0, temp).concat([...statsArray].slice(temp+1))
-        setSelectedOption(e)
+        setSelectedOption(chosenArray)
         setStatsArray(newArray)
     }
     const stats = statNames.map(item => (
         <Category handleSelect={handleChange}
-                          defaultValue={selectedOption}
-                          options={statsArray} 
-                          name={item}  />
+                  defaultValue={selectedOption}
+                  options={statsArray}
+                  id={item} 
+                  name={item}  />
     ))
     return (
         <div className='Investigator, Investigator--Characteristics'>
